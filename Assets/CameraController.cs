5dcd34fc393275503ee6fbe4a6c55b77228db30a
CobaltBlue73 <md.zaid73@gmail.com>
2020-09-09 08:19:48 +0800

    Modified Simple AR for test bed

---
+++ Assets/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Camera arCamera;
+    [SerializeField] private ARCameraBackground arCameraBackground;
+    [SerializeField] private RawImage previewImage;
+    [SerializeField] private int maxSize = 512;
+
+    private Texture2D _lastCameraTexture;
+    
+    public void TakeNormalPic()
+    {
+        if(NativeCamera.IsCameraBusy()) return;
+        
+        NativeCamera.Permission permission = NativeCamera.TakePicture( ( path ) =>
+        {
+            Debug.Log( "Image path: " + path );
+            if( path != null )
+            {
+                // Create a Texture2D from the captured image
+                previewImage.texture = NativeCamera.LoadImageAtPath( path, maxSize );
+            }
+        }, maxSize );
+
+        Debug.Log( "Permission result: " + permission );
+    }
+
+    public void TakeARPic()
+    {
+        previewImage.texture = CameraCapturer.Capture(arCamera);
+
+    }
+}
